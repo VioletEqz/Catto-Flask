d0f9dd7cbcefdaa6d558253095130eee2e16e258
Violet <violet.eqz@gmail.com>
2021-07-23 14:41:30 +0700

    nice

--- models.py
+++ models.py
@@ -1,7 +1,6 @@
 # The class containing the model
-import onnxruntime,onnx
+import onnxruntime
 import numpy as np
-from matplotlib import pyplot as plt
 from utils import *
 
 class Catto:
@@ -26,3 +25,24 @@ class Catto:
         preds = non_max_suppression(np.array(preds))[0]
         return preds
 
+class Face:
+    def __init__(self):
+        self.model = onnxruntime.InferenceSession('Catto-Face.onnx', None)
+    
+    def preprocess(self,input):
+        cv2img = cv2.imread(input)
+        img = letterbox(cv2img,scaleFill=False,auto=False)[0]
+        # Conversion
+        img = img[:, :, ::-1].transpose(2, 0, 1)  # BGR to RGB, to 3x640x640
+        img = img.astype(np.float32)
+        img = np.ascontiguousarray(img)
+        self.original = img
+        img /= 255.0  # 0 - 255 to 0.0 - 1.0
+        img = np.expand_dims(img,0)
+        return img
+
+    def infer(self,input):
+        img = self.preprocess(input)
+        preds = (self.model.run([self.model.get_outputs()[0].name], {self.model.get_inputs()[0].name: img}))
+        preds = non_max_suppression(np.array(preds))[0]
+        return preds
